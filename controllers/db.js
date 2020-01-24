fa0666cec0f822ab21381cbb2d21652c4a245929
Wachiye <47672963+Wachiye@users.noreply.github.com>
2020-01-24 03:10:05 -0800

    Update db.js

--- controllers/db.js
+++ controllers/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 
 //connection string
 let config = {
-    host: "https://www.db4free.net:3306/",
+    host: "db4free.net",
     user: "expdblog",
     password: "expdblog",
     database: "expdblog"
